9f82221d7a53dfdd9463f965957116613116f5e6
Jeremiah Ikechukwu Obuseri <ikechukwujerristar@gmail.com>
2024-10-22 14:34:14 +0000

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,44 @@
-#!/usr/bin/python3
+!/usr/bin/python3
+"""
+Log parsing
+"""
+import sys
 
-from sys import stdin
 
-
-def printstats(file_size, status_codes):
+def print_metrics(file_size, status_codes):
     """
-    This prints statistics at the beginning and every 10 lines
-    This will also be called on a Keyboard interruption
+    Print metrics
     """
-    print("File size: " + str(file_size))
-    for code in sorted(status_codes.keys()):
+    print("File size: {}".format(file_size))
+    codes_sorted = sorted(status_codes.keys())
+    for code in codes_sorted:
         if status_codes[code] > 0:
-            print(code + ": " + str(status_codes[code]))
+            print("{}: {}".format(code, status_codes[code]))
 
 
-line_num = 0
-file_size = 0
-status_code = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                "403": 0, "404": 0, "405": 0, "500": 0}
+codes_count = {'200': 0, '301': 0, '400': 0, '401': 0,
+               '403': 0, '404': 0, '405': 0, '500': 0}
+file_size_total = 0
+count = 0
 
-try:
-    for line in stdin:
-        line_num += 1
-        split_line = line.split()
-
-        if len(split_line) > 1:
-            file_size += int(split_line[-1])
-
-        if len(split_line) > 2 and split_line[-2].isnumeric():
-            status_code = split_line[-2]
-        else:
-            status_code = 0
-
-        if status_code in status_codes.keys():
-            status_codes[status_code] += 1
-
-        if line_num % 10 == 0:
-            printstats(file_size, status_codes)
-
-    printstats(file_size, status_codes)
-
-except (KeyboardInterrupt):
-    printstats(file_size, status_codes)
-    raise)
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            try:
+                status_code = line.split()[-2]
+                if status_code in codes_count.keys():
+                    codes_count[status_code] += 1
+                # Grab file size
+                file_size = int(line.split()[-1])
+                file_size_total += file_size
+            except Exception:
+                pass
+            # print metrics if 10 lines have been read
+            count += 1
+            if count == 10:
+                print_metrics(file_size_total, codes_count)
+                count = 0
+    except KeyboardInterrupt:
+        print_metrics(file_size_total, codes_count)
+        raise
+   print_metrics(file_size_total, codes_count)
